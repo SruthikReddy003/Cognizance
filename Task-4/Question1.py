94e9b38511bb6832187db456a79a311a7170129e
SruthikReddy <95969937+SruthikReddy003@users.noreply.github.com>
2022-02-21 11:48:03 +0530

    Question1.py

--- Task-4/Question1.py
+++ Task-4/Question1.py
@@ -1,4 +1,4 @@
-a = int(input("Enter the first number "))
-b = int(input("Enter the second number"))
+a = int(input("Enter the first number :"))
+b = int(input("Enter the second number :"))
 c = a + b
 print("Sum of a & b is: ", c)
